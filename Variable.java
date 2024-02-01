1bb520e1987339cbf7ae693cb499d0d84fd0141b
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-01 23:19:54 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,11 @@
+class Variable {
+public static void main(String[] args) {
+int a = 5 ;
+int b = 11; 
+System.out.print(a+b);
+String s = "I AM A" ;
+String w = "STUDENT" ;
+String E = s+w ;
+System.out.println(E);
+}
+}
