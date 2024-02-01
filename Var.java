bc6b8d73b8d09a44739cc74d0d072e9717680564
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-01 23:08:55 +0530

    Add files via upload

---
+++ Var.java
@@ -0,0 +1,15 @@
+class Var {
+public static void main(String[] args) {
+
+//String Creation
+
+String S = new String("BAS");
+System.out.println(S);
+int Num = 55 ;
+char c = 'B';
+boolean bool = false ;
+System.out.println(Num);
+System.out.println(c);
+System.out.println(bool);
+}
+}
