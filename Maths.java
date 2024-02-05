f2119a1c93615fa77201eb2c3e587f3fc50ccd50
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-05 21:45:11 +0530

    Add files via upload

---
+++ Maths.java
@@ -0,0 +1,9 @@
+
+class Maths {
+public static void main(String[] args){
+System.out.println(Math.max(30,3));
+System.out.println(Math.min(30,3));
+System.out.println(Math.sqrt(49));
+System.out.println(Math.abs(4.344));
+}
+}
---
+++ Simple.java
@@ -0,0 +1,19 @@
+class Simple {
+public static void main(String[] args) {
+String s = "HEllo" ;
+String d = "Diva" ;
+int num = 77 ;
+System.out.println(s.toUpperCase());
+System.out.println(s.indexOf("E"));
+System.out.println(s.charAt(2));
+System.out.println(num/11);
+System.out.println(s + d + "USING +");
+String Final = s.concat(d);
+System.out.println(Final);
+System.out.println("HELLO \n NEXT"); 
+System.out.println("HELLO \r NEXT");
+System.out.println("HELLO \t NEXT");
+System.out.println("HELLO \b NEXT");
+System.out.println("HELLO \f NEXT");
+}
+}
