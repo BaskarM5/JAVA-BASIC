014fdae3648d3971bc3f5990893fb5b2bf13a444
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-07 21:21:14 +0530

    Add files via upload

---
+++ Brkcnt.java
@@ -0,0 +1,13 @@
+class Brkcnt {
+public static void main(String[] args) {
+for(int i = 55;i<60;i++) {
+if (i==57) {
+continue;
+}
+System.out.println(i);
+
+if (i==58) {
+break ; }
+}
+}
+}
---
+++ Floop.java
@@ -0,0 +1,9 @@
+class Floop {
+public static void main(String[] args) {
+int i, r = 50;
+for(i=55;i>r;) {
+System.out.println(r);
+r++ ;
+}
+}
+}
