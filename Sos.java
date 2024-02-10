6bcc6593a3c3554daf24f86421dc9eb71925cae0
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-10 21:28:01 +0530

    Add files via upload

---
+++ Sos.java
@@ -0,0 +1,14 @@
+class Sos { 
+public static void main(String[] args) { 
+int i ;
+String s = "NEW" ;
+char[] lov = {'l','o','v','e'};
+String W = new String(lov);
+System.out.println(s);
+System.out.println(W);
+for(i = 0;i<W.length();i++)
+ {
+System.out.println(W.charAt(i));
+}
+}
+}
