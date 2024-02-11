43a2081dd483165b499452e5c7fdb5a2dd7306b6
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-11 21:08:04 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.Scanner ;
+class Input {
+public static void main(String[] args) {
+int n;
+System.out.println("Enter A Number");
+Scanner s = new Scanner(System.in);
+n = s.nextInt();
+System.out.println(++n);
+}
+}
