84864ab39eb648fc4ad5ffc3857c8e94f6e5fe3a
Baskar M <99343089+BaskarM5@users.noreply.github.com>
2024-02-15 21:27:17 +0530

    Add files via upload

---
+++ valan.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class valan {
+public static void main(String[] args) {
+int age ;
+
+String name;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter Your Name:");
+name = sc.nextLine();
+System.out.println(name+ " Enter Your Age:");
+age = sc.nextInt();
+
+System.out.println("Thank You!!");
+}
+}
